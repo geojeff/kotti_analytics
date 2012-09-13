588623262ac54cb55d068da3def64aebe4243705
Jeff Pittman <geojeff@me.com>
2012-09-13 05:51:13 -0500

    Updated to use assign_slot instead of register, and related changes.

--- kotti_analytics/__init__.py
+++ kotti_analytics/__init__.py
@@ -1,8 +1,4 @@
-from pyramid.renderers import render
-
 from kotti.util import extract_from_settings
-from kotti.views.slots import register
-from kotti.views.slots import RenderBelowContent
 
 ANALYTICS_WIDGET_DEFAULTS = {
     'tracking_id': '',
@@ -14,7 +10,10 @@ def render_analytics_widget(context, request, name=''):
         prefix += name + '.'
     variables = ANALYTICS_WIDGET_DEFAULTS.copy()
     variables.update(extract_from_settings(prefix))
-    return render('templates/analytics_widget.pt', variables)
+    return variables
 
-def include_widget(config, where=RenderBelowContent):
-    register(where, None, render_analytics_widget)
+def include_widget(config, where='right'): # pragma: no cover
+    config.add_view(render_widget,
+                    name='analytics',
+                    renderer='templates/analytics_widget.pt')
+    assign_slot('analytics', where)
